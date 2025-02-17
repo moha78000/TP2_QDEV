a9bb61a7aa08c6600e843f8c3c0ee0344a1b6ff9
22302683 <moh78078000@gmail.com>
2025-02-17 10:06:48 +0100

    "Ajout de classe test"

---
+++ src/test/DateTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateTest {
+
+    @org.junit.jupiter.api.Test
+    void estBissextile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dernierJourDuMois() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void estValide() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+    }
+}
