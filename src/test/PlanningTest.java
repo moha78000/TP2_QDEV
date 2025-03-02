94208982498b7bed485b2cc9ec9bc7bf1a309aef
Khaldi Mohamed <moh78078000@gmail.com>
2025-03-02 11:42:08 +0100

    Ajout des tests pour la classe Planning

--- src/test/PlanningTest.java
+++ src/test/PlanningTest.java
@@ -10,9 +10,9 @@ class PlanningTest {
     @Test
     void ajout() {
         Planning p1 = new Planning(2);
-        Reservation res1 = new Reservation(new Date(10, 3, 2025), new PlageHoraire(new Horaire(10, 0), new Horaire(11, 0)), "Foot");
-        Reservation res2 = new Reservation(new Date(11, 3, 2025), new PlageHoraire(new Horaire(12, 0), new Horaire(13, 0)), "Judo");
-        Reservation res3 = new Reservation(new Date(12, 3, 2025), new PlageHoraire(new Horaire(9, 0), new Horaire(10, 0)), "Karaté");
+        Reservation res1 = new Reservation(new Date(10, 3, 2025), new PlageHoraire(new Horaire(10, 0), new Horaire(12, 0)), "Foot");
+        Reservation res2 = new Reservation(new Date(11, 3, 2025), new PlageHoraire(new Horaire(12, 0), new Horaire(14, 0)), "Judo");
+        Reservation res3 = new Reservation(new Date(12, 3, 2025), new PlageHoraire(new Horaire(9, 0), new Horaire(11, 0)), "Karaté");
         assertTrue(p1.ajout(res1));
         assertTrue(p1.ajout(res2));
         assertFalse(p1.ajout(res3));
