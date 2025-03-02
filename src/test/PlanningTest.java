fd96fd0d7f7a6ecc0bc3ae27829c9f47754aad4a
Khaldi Mohamed <moh78078000@gmail.com>
2025-03-02 11:44:09 +0100

    Ajout des tests pour la classe Planning sur la méthode getReservation

--- src/test/PlanningTest.java
+++ src/test/PlanningTest.java
@@ -21,6 +21,12 @@ class PlanningTest {
 
     @Test
     void getReservation() {
+        Planning planning = new Planning(3);
+        Reservation res = new Reservation(new Date(15, 3, 2025), new PlageHoraire(new Horaire(10, 0), new Horaire(12, 0)), "Réunion");
+        planning.ajout(res);
+
+        assertEquals(res, planning.getReservation(new Date(15, 3, 2025)));
+        assertNull(planning.getReservation(new Date(16, 3, 2025)));
     }
 
     @Test
