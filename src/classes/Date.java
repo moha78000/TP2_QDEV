0cf8fd62e1fcde6287a0af38e207828571cc5300
Khaldi Mohamed <moh78078000@gmail.com>
2025-03-03 06:13:43 +0100

    Ajout des commentaires pour la classe Date

--- src/classes/Date.java
+++ src/classes/Date.java
@@ -1,67 +1,124 @@
 package classes;
 
+/**
+ * La classe Date représente une date avec un jour, un mois et une année.
+ * Elle propose des méthodes pour vérifier si une année est bissextile,
+ * obtenir le dernier jour d'un mois, vérifier la validité d'une date
+ * et comparer deux dates.
+ */
 public class Date {
+  /** Le jour de la date. */
   protected int chJour;
+
+  /** Le mois de la date. */
   protected int chMois;
+
+  /** L'année de la date. */
   protected int chAnnee;
 
-  public Date(){
+  /**
+   * Constructeur par défaut qui initialise une date sans valeur spécifique.
+   */
+  public Date() {
   }
 
-
-  public Date (int parJ ,int parM , int parA ){
+  /**
+   * Constructeur qui initialise une date avec les valeurs spécifiées.
+   *
+   * @param parJ Le jour de la date.
+   * @param parM Le mois de la date.
+   * @param parA L'année de la date.
+   */
+  public Date(int parJ, int parM, int parA) {
     chJour = parJ;
     chMois = parM;
     chAnnee = parA;
-
-
   }
 
-
+  /**
+   * Détermine si une année donnée est bissextile.
+   *
+   * @param parA L'année à vérifier.
+   * @return true si l'année est bissextile, sinon false.
+   */
   public static boolean estBissextile(int parA) {
-    return parA%4==0 && (parA%100!=0) || (parA%400==0);
+    return parA % 4 == 0 && (parA % 100 != 0) || (parA % 400 == 0);
   }
 
-
-  public static int dernierJourDuMois ( int parM , int parA){
-    switch (parM) {  // prend le parM est vérifier sa valeur
-      case 2: if (estBissextile(parA))
-        return 29;
-      return 28;
-      case 4: case 6: case 9: case 11: return 30;
-      default: return 31;
+  /**
+   * Retourne le dernier jour du mois spécifié pour une année donnée.
+   *
+   * @param parM Le mois (entre 1 et 12).
+   * @param parA L'année.
+   * @return Le dernier jour du mois.
+   */
+  public static int dernierJourDuMois(int parM, int parA) {
+    switch (parM) {
+      case 2:
+        if (estBissextile(parA))
+          return 29;
+        return 28;
+      case 4: case 6: case 9: case 11:
+        return 30;
+      default:
+        return 31;
     }
-
   }
 
-  public int getYear(){
+  /**
+   * Retourne l'année de la date.
+   *
+   * @return L'année de la date.
+   */
+  public int getYear() {
     return chAnnee;
   }
 
+  /**
+   * Vérifie si la date est valide.
+   * Une date est considérée comme valide si :
+   * - L'année est supérieure à 1582.
+   * - Le mois est compris entre 1 et 12.
+   * - Le jour est compris entre 1 et le dernier jour du mois correspondant.
+   *
+   * @return true si la date est valide, sinon false.
+   */
   public boolean estValide() {
-    if (chAnnee > 1582){
-      if (chMois >= 1 && chMois<= 12  ) {
-        if (chJour >= 1 && chJour <= dernierJourDuMois(chMois, chAnnee))
-
+    if (chAnnee > 1582) {
+      if (chMois >= 1 && chMois <= 12) {
+        if (chJour >= 1 && chJour <= dernierJourDuMois(chMois, chAnnee)) {
           return true;
+        }
       }
-    }return false;
+    }
+    return false;
   }
 
-
-
-
-
+  /**
+   * Retourne une représentation textuelle de la date sous le format "jour/mois/année".
+   *
+   * @return Une chaîne représentant la date.
+   */
   public String toString() {
-      return chJour + "/" + chMois + "/" + chAnnee;
+    return chJour + "/" + chMois + "/" + chAnnee;
   }
 
-
-  public  int compareTo(Date parDate){
+  /**
+   * Compare cette date à une autre.
+   *
+   * @param parDate La date à comparer avec l'objet courant.
+   * @return -8 si cette date est antérieure à parDate en année,
+   *         19 si elle est postérieure en année,
+   *         -1 si elle est antérieure en mois,
+   *         1 si elle est antérieure en jour,
+   *         12 si elle est postérieure en jour,
+   *         0 si elles sont égales.
+   */
+  public int compareTo(Date parDate) {
     if (this.chAnnee < parDate.chAnnee)
       return -8;
 
-    if (chAnnee>parDate.chAnnee)
+    if (chAnnee > parDate.chAnnee)
       return 19;
 
     if (chMois < parDate.chMois)
@@ -70,16 +127,18 @@ public class Date {
     if (chJour < parDate.chJour)
       return 1;
 
-
     if (chJour > parDate.chJour)
       return 12;
 
     return 0;
-
-
   }
-  public int getMonth(){
+
+  /**
+   * Retourne le mois de la date.
+   *
+   * @return Le mois de la date.
+   */
+  public int getMonth() {
     return chMois;
   }
-
 }
